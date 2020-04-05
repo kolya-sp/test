86275529d8dc3d98a5a42a6dd19c6152e56b1642
kolya-sp <kfrjvrf@i.ua>
2020-04-05 23:24:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 2, 4, 6, 8, 10 };
+
+            IEnumerator ie = numbers.GetEnumerator(); // получаем IEnumerator
+            int it = (int)ie.Current;
+            Console.WriteLine(it);
+            while (ie.MoveNext())   // пока не будет возвращено false
+            {
+                int item = (int)ie.Current;     // берем элемент на текущей позиции
+                Console.WriteLine(item);
+            }
+            ie.Reset(); // сбрасываем указатель в начало массива
+            Console.Read();
+        }
+    }
+}
